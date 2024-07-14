840150b3be17a973940b9af508e59b835fc1b9d7
fjql <98269704+fjql@users.noreply.github.com>
2024-07-14 22:57:28 +0800

    Created todo grid and items

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+  import "$lib/reset.css";
+</script>
+
+<header>
+  <h1>dodododo</h1>
+</header>
+
+<slot/>
+
+<style>
+  header {
+    position: relative;
+    left: 50%;
+    translate: -50% 0%;
+
+    width: 80dvw;
+    padding: 0rem 1rem;
+
+    @media screen and (max-width: 805px) {
+      text-align: center;
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let todos = $state([
+    { title: "1", text: "", done: false },
+    { title: "2", text: "", done: false },
+    { title: "3", text: "", done: false },
+    { title: "4", text: "", done: false },
+    { title: "5", text: "", done: false },
+    { title: "6", text: "", done: false },
+  ]);
+</script>
+
+<main>
+  {#each todos as todo}
+    <div class="todo">
+      <header>
+        <h1>{todo.title}</h1>
+        <input type="checkbox" value={todo.done}/>
+      </header>
+      <p>{todo.text}</p>
+    </div>
+  {/each}
+  <div class="todo">
+    add
+  </div>
+</main>
+
+<style>
+  main {
+    position: relative;
+    left: 50%;
+    translate: -50% 0%;
+
+    height: 100%;
+    width: 80dvw;
+    padding: .5rem;
+
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(10vmax, 30vmax));
+    gap: .25rem;
+    justify-content: center;
+    align-items: stretch;
+  }
+
+  .todo {
+    border: 2px solid rgb(19, 15, 15);
+    padding: .5rem 1rem;
+
+    header {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+    }
+  }
+</style>
